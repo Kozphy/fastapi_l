bb27b73a8986fd762ead245bb21dea898c776a4d
Zisxa <dbdf0147@gmail.com>
2022-06-10 17:49:20 +0800

    add procfile

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import (FastAPI, HTTPException,
 Depends, Response, status)
+from fastapi.middleware.cors import CORSMiddleware
 
 from configuration.api_service_config.config_fastapi import settings
 
@@ -8,6 +9,22 @@ from routers import post, user, auth, vote
 
 
 app = FastAPI()
+
+origins = [
+    "https://www.google.com",
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
@@ -18,7 +35,7 @@ app.include_router(vote.router)
 # home
 @app.get("/")
 async def root():
-    print(settings)
+    # print(settings)
     return {"message": "Hello World"}
 
     
