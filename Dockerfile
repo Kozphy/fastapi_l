e3844ad2cd1d8fb0d36e8313868d029c8d09dcd4
Zixas <dbdf0147@gmail.com>
2022-08-19 15:01:57 +0000

    update

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.10
 
-WORKDIR /src
+WORKDIR /workspace
 
 COPY requirements.txt ./
 
--- testcpp/src/main.cpp
+++ testcpp/src/main.cpp
@@ -9,7 +9,6 @@
 #include "spdlog/spdlog.h"
 #include <boost/lambda/lambda.hpp>
 import Example;
-import std.core;
 
 
 using namespace std;
